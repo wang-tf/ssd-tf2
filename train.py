3ca2a51d8abcfa54d9e2d62b449c2bf7fd3063f7
wangtf <wangtf@glodon.com>
2020-03-16 11:42:38 +0800

    format

--- train.py
+++ train.py
@@ -11,10 +11,9 @@ from anchor import generate_default_boxes
 from network import create_ssd
 from losses import create_losses
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--data-dir', default='../dataset')
-parser.add_argument('--data-year', default='2007')
+parser.add_argument('--data-year', default='2012')
 parser.add_argument('--arch', default='ssd300')
 parser.add_argument('--batch-size', default=32, type=int)
 parser.add_argument('--num-batches', default=-1, type=int)
@@ -25,7 +24,7 @@ parser.add_argument('--weight-decay', default=5e-4, type=float)
 parser.add_argument('--num-epochs', default=120, type=int)
 parser.add_argument('--checkpoint-dir', default='checkpoints')
 parser.add_argument('--pretrained-type', default='base')
-parser.add_argument('--gpu-id', default='0')
+parser.add_argument('--gpu-id', default='2')
 
 args = parser.parse_args()
 
@@ -39,8 +38,7 @@ def train_step(imgs, gt_confs, gt_locs, ssd, criterion, optimizer):
     with tf.GradientTape() as tape:
         confs, locs = ssd(imgs)
 
-        conf_loss, loc_loss = criterion(
-            confs, locs, gt_confs, gt_locs)
+        conf_loss, loc_loss = criterion(confs, locs, gt_confs, gt_locs)
 
         loss = conf_loss + loc_loss
         l2_loss = [tf.nn.l2_loss(t) for t in ssd.trainable_variables]
@@ -53,7 +51,7 @@ def train_step(imgs, gt_confs, gt_locs, ssd, criterion, optimizer):
     return loss, conf_loss, loc_loss, l2_loss
 
 
-if __name__ == '__main__':
+def main():
     os.makedirs(args.checkpoint_dir, exist_ok=True)
 
     with open('./config.yml') as f:
@@ -67,15 +65,22 @@ if __name__ == '__main__':
     default_boxes = generate_default_boxes(config)
 
     batch_generator, val_generator, info = create_batch_generator(
-        args.data_dir, args.data_year, default_boxes,
+        args.data_dir,
+        args.data_year,
+        default_boxes,
         config['image_size'],
-        args.batch_size, args.num_batches,
-        mode='train', augmentation=['flip'])  # the patching algorithm is currently causing bottleneck sometimes
-    
+        args.batch_size,
+        args.num_batches,
+        mode='train',
+        augmentation=[
+            'flip'
+        ])  # the patching algorithm is currently causing bottleneck sometimes
+
     try:
-        ssd = create_ssd(NUM_CLASSES, args.arch,
-                        args.pretrained_type,
-                        checkpoint_dir=args.checkpoint_dir)
+        ssd = create_ssd(NUM_CLASSES,
+                         args.arch,
+                         args.pretrained_type,
+                         checkpoint_dir=args.checkpoint_dir)
     except Exception as e:
         print(e)
         print('The program is exiting...')
@@ -85,14 +90,17 @@ if __name__ == '__main__':
 
     steps_per_epoch = info['length'] // args.batch_size
 
-    lr_fn = PiecewiseConstantDecay(
-        boundaries=[int(steps_per_epoch * args.num_epochs * 2 / 3),
-                    int(steps_per_epoch * args.num_epochs * 5 / 6)],
-        values=[args.initial_lr, args.initial_lr * 0.1, args.initial_lr * 0.01])
-    
-    optimizer = tf.keras.optimizers.SGD(
-        learning_rate=lr_fn,
-        momentum=args.momentum)
+    lr_fn = PiecewiseConstantDecay(boundaries=[
+        int(steps_per_epoch * args.num_epochs * 2 / 3),
+        int(steps_per_epoch * args.num_epochs * 5 / 6)
+    ],
+                                   values=[
+                                       args.initial_lr, args.initial_lr * 0.1,
+                                       args.initial_lr * 0.01
+                                   ])
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_fn,
+                                        momentum=args.momentum)
 
     train_log_dir = 'logs/train'
     val_log_dir = 'logs/val'
@@ -111,20 +119,25 @@ if __name__ == '__main__':
             avg_conf_loss = (avg_conf_loss * i + conf_loss.numpy()) / (i + 1)
             avg_loc_loss = (avg_loc_loss * i + loc_loss.numpy()) / (i + 1)
             if (i + 1) % 50 == 0:
-                print('Epoch: {} Batch {} Time: {:.2}s | Loss: {:.4f} Conf: {:.4f} Loc: {:.4f}'.format(
-                    epoch + 1, i + 1, time.time() - start, avg_loss, avg_conf_loss, avg_loc_loss))
+                print(
+                    'Epoch: {} Batch {} Time: {:.2}s | Loss: {:.4f} Conf: {:.4f} Loc: {:.4f}'
+                    .format(epoch + 1, i + 1,
+                            time.time() - start, avg_loss, avg_conf_loss,
+                            avg_loc_loss))
 
         avg_val_loss = 0.0
         avg_val_conf_loss = 0.0
         avg_val_loc_loss = 0.0
         for i, (_, imgs, gt_confs, gt_locs) in enumerate(val_generator):
             val_confs, val_locs = ssd(imgs)
-            val_conf_loss, val_loc_loss = criterion(
-                val_confs, val_locs, gt_confs, gt_locs)
+            val_conf_loss, val_loc_loss = criterion(val_confs, val_locs,
+                                                    gt_confs, gt_locs)
             val_loss = val_conf_loss + val_loc_loss
             avg_val_loss = (avg_val_loss * i + val_loss.numpy()) / (i + 1)
-            avg_val_conf_loss = (avg_val_conf_loss * i + val_conf_loss.numpy()) / (i + 1)
-            avg_val_loc_loss = (avg_val_loc_loss * i + val_loc_loss.numpy()) / (i + 1)
+            avg_val_conf_loss = (avg_val_conf_loss * i +
+                                 val_conf_loss.numpy()) / (i + 1)
+            avg_val_loc_loss = (avg_val_loc_loss * i +
+                                val_loc_loss.numpy()) / (i + 1)
 
         with train_summary_writer.as_default():
             tf.summary.scalar('loss', avg_loss, step=epoch)
@@ -138,4 +151,9 @@ if __name__ == '__main__':
 
         if (epoch + 1) % 10 == 0:
             ssd.save_weights(
-                os.path.join(args.checkpoint_dir, 'ssd_epoch_{}.h5'.format(epoch + 1)))
+                os.path.join(args.checkpoint_dir,
+                             'ssd_epoch_{}.h5'.format(epoch + 1)))
+
+
+if __name__ == '__main__':
+    main()
